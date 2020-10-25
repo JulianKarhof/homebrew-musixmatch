817008a10e5b3ee7a80f03272dace765988f6c67
Julian Karhof <julian@karhof.net>
2020-10-25 10:09:16 +0100

    refactor: remove versioning from download

--- Casks/musixmatch.rb
+++ Casks/musixmatch.rb
@@ -1,8 +1,8 @@
 cask 'musixmatch' do
-    version '0.21.27'
-    sha256 'cb4f789b2e50ca89ffa1b135ec579c249ade4b513c9256958194cfe4661b9325'
+    version :latest
+    sha256 :no_check
   
-    url "https://download-app.musixmatch.com/download/Musixmatch-#{version}.dmg"
+    url "https://download-app.musixmatch.com/download/musixmatch.dmg"
     appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=http://download-app.musixmatch.com/download/osx'
     name 'Musixmatch'
     homepage 'https://www.musixmatch.com/'
